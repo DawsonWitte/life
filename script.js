c31e6e4ab442e723ad72fa35a1a4de8ac95f1dac
Dawson <dawson.witte.dev@gmail.com>
2022-01-17 11:35:46 -0500

    Added Jquery, able to get cell number on click

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-
 //sets dimensions of the play area grid
 let dimension = 10;
+let color = document.querySelector('.color')
+let j = 0;
 
-let run = "false";
+let draw = false;
+let run = false;
+
+const cellsArray = [];
+const lifeArray = [];
 
 //button to test the function
 const btn = document.querySelector('#btn');
@@ -12,19 +17,36 @@ btn.addEventListener('click', () => {
 
 //run to create the grid
 function gridCreation() {
+    j = 0;
     //creates columns
     for (let i = 0; i < dimension; i++) {
-        const column = document.createElement('column'+1);
+        const column = document.createElement('column'+i);
         column.classList.add('column');
 
         container.appendChild(column);
 
         //creates rows within each column
         for (let i = 0; i < dimension; i++) {
-            const row = document.createElement('row'+i);
+            const row = document.createElement("z_" + j);
             row.classList.add('row');
+            row.classList.add(j);
+            lifeArray.push(0);
+            cellsArray.push("z_" + j);
 
-            column.appendChild(row)
+
+            row.addEventListener('click', function(){
+                row.style.backgroundColor = 'black'
+                var theClass = $(this).attr('class');
+                cellNumber = (theClass.split(" ").pop())
+                lifeArray[cellNumber]=1;
+                console.log(lifeArray[cellNumber])
+
+            })
+            column.appendChild(row);
+            j++;
         }
     }
 }
+
+//cellName = "z_8";
+//cellNumber = (cellName.split("_").pop());
